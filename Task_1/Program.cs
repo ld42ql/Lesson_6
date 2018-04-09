a4c406cf47c0e76d915cfe78f89010455aeb16aa
Vladimir baleev <ld42ql@hotmail.com>
2018-04-09 09:29:44 +0300

    Выполнено задание №1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task_1
+﻿namespace Task_1
 {
+    #region Задание №1
+    // Реализовать простейшую хэш-функцию.На вход функции подается строка, на выходе получается сумма кодов символов
+    #endregion
     class Program
     {
+        /// <summary>
+        /// Простейшую хэш-функцию, на вход подается строка, на выходе получается сумма кодов символов Unicode
+        /// </summary>
+        static double Convert(string str)
+        {
+            double res = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                res += System.Convert.ToChar(str[i]);
+            }
+
+            return res;
+        }
+
         static void Main(string[] args)
         {
+            System.Console.Write("Введите строку: ");
+
+            string str = System.Console.ReadLine();
+
+            System.Console.WriteLine($"{Convert(str)}");
+
+            System.Console.ReadKey();
         }
     }
 }
